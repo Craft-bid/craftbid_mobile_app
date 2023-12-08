536e1ea5e7966090d519f2d06cd535d0008e2bcb
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-12-08 11:18:17 +0100

    poprawki

--- app/src/main/java/com/pl/craftbidapp/di/RepositoryModule.kt
+++ app/src/main/java/com/pl/craftbidapp/di/RepositoryModule.kt
@@ -7,9 +7,6 @@ import com.pl.craftbidapp.data.repository.OfferListRepositoryImpl
 import com.pl.craftbidapp.domain.repository.AuthRepository
 import com.pl.craftbidapp.domain.repository.CreateListingRepository
 import com.pl.craftbidapp.data.repository.BidRepositoryImpl
-import com.pl.craftbidapp.data.repository.MyRepositoryImpl
-import com.pl.craftbidapp.data.repository.OfferListRepositoryImpl
-import com.pl.craftbidapp.domain.repository.AuthRepository
 import com.pl.craftbidapp.domain.repository.BidRepository
 import com.pl.craftbidapp.domain.repository.MyRepository
 import com.pl.craftbidapp.domain.repository.OfferListRepository
@@ -22,12 +19,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-
-    @Binds
-    @Singleton
-    abstract fun bindMyRepository(
-        myRepositoryImpl: MyRepositoryImpl
-    ): MyRepository
 
     @Binds
     @Singleton
@@ -52,4 +43,10 @@ abstract class RepositoryModule {
     abstract fun bindBidRepository(
         offerListRepositoryImpl: BidRepositoryImpl
     ): BidRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindMyRepository(
+        myRepositoryImpl: MyRepositoryImpl
+    ): MyRepository
 }
